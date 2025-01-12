8794425fc7a4861d82b308ef3504dbf630c572df
Martin Macicha <macima@delta-studenti.cz>
2025-01-12 21:19:24 +0000

    ✨ Implemented sensor history

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import json
+import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+uri = os.getenv('MONGODB_URI')
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client.roomtemp
+
+temperatureCollection = db["temperatures"]
+humidityCollection = db["humidities"]
+
 
 alive = 0
 data = {}
@@ -18,7 +32,21 @@ def landing():
 def sensors():
     return render_template("sensors.html")
 
+@app.route("/history")
+def history():
+    temperatures = []
+    humidities = []
 
+    for reading in temperatureCollection.find().sort("Time", -1):
+        reading["Temperature"] = str(reading['Temperature'])
+        reading["Time"] = reading['Time'].strftime("%b %d %Y %H:%M")
+        temperatures.append(reading)
+    for reading in humidityCollection.find().sort("Time", -1):
+        reading["Humidity"] = str(reading['Humidity'])
+        reading["Time"] = reading['Time'].strftime("%b %d %Y %H:%M")
+        humidities.append(reading)
+
+    return render_template("history.html", temperatures = temperatures, humidities = humidities)
 
 
 @app.route("/keep_alive")
